fa9cb56216899fa8e3d1b18b68a29714636fee64
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-04-06 16:18:49 -0400

    Fix auth check skipping

--- haldane/basic_auth.py
+++ haldane/basic_auth.py
@@ -7,18 +7,19 @@ from haldane.config import Config
 from haldane.log import getLogger
 
 
+def skip_check():
+    """This function allows us to skip basic auth check
+    for requests coming from specific allowed ips
+    """
+    logger = getLogger('haldane')
+    provided_ips = request.access_route
+    return len(provided_ips) > 0 and provided_ips[0] in Config.ALLOWED_IPS
+
+
 def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    logger = getLogger('haldane')
-    provided_ips = request.access_route
-
-    logger.info('Provided IPs: {0}'.format(provided_ips))
-    logger.info('Allowed IPs: {0}'.format(Config.ALLOWED_IPS))
-    if len(provided_ips) > 0 and provided_ips[0] in Config.ALLOWED_IPS:
-        return True
-
     authentications = Config.BASIC_AUTH.split(',')
     authentication = '{0}:{1}'.format(username, password)
 
@@ -36,7 +37,7 @@ def authenticate():
 def requires_auth(f):
     @functools.wraps(f)
     def decorated(*args, **kwargs):
-        if not Config.BASIC_AUTH:
+        if skip_check() or not Config.BASIC_AUTH:
             return f(*args, **kwargs)
 
         auth = request.authorization
