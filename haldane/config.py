6a6e5acf11ce995341d819970d22a072f3311c9a
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2016-06-08 18:03:42 -0400

    Filter out empty strings from config lists

--- haldane/config.py
+++ haldane/config.py
@@ -3,10 +3,10 @@ from haldane.utils import to_bool
 
 
 class Config(object):
-    ALLOWED_IPS = os.getenv('ALLOWED_IPS').split(',')
+    ALLOWED_IPS = filter(None, os.getenv('ALLOWED_IPS').split(','))
     ALTERNATIVE_AUTOSCALE_TAG_NAME = os.getenv('ALTERNATIVE_AUTOSCALE_TAG_NAME', None)
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_REGIONS = os.getenv('AWS_REGIONS', 'us-east-1').split(',')
+    AWS_REGIONS = filter(None, os.getenv('AWS_REGIONS', 'us-east-1').split(','))
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     BASIC_AUTH = os.getenv('BASIC_AUTH')
     BOOLEAN_AWS_TAG_ATTRIBUTES = filter(None, os.getenv('BOOLEAN_AWS_TAG_ATTRIBUTES', '').split(','))
